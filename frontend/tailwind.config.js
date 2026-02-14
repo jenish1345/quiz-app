1f4554785328a006f3d0a48e60a17f67cd108bb3
jenish1345 <antonyjenishfernando.27it@licet.ac.in>
2026-02-14 10:14:15 +0530

    Update tailwind.config.js
    color changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,8 +6,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#6366f1',
-        secondary: '#8b5cf6'
+        primary: '#63b6f1ff',
+        secondary: '#d0eef5ff'
       }
     }
   },
